ad309894be8763f71f8929c41acc7e7ce9b2ecde
VivianSolide <vivian.sarazin@gmail.com>
2018-02-21 11:32:21 +0100

    iteration #0

--- api.js
+++ api.js
@@ -2,8 +2,23 @@ const service = axios.create({
   baseURL: 'http://localhost:3000/movies',
 });
 
-const api = {
-  getAll: () => null, // Return instead a promise containing all the movies,
+const api = { // Return instead a promise containing all the movies,
+  getAll: () => {
+    return service
+    .get('/')
+    .then(movies => {
+      return movies;
+      console.log(movies);
+    })
+    .catch(err => {
+      console.error(err);
+      throw err;
+    })
+  }, 
+    
+
+
+
   getOne: id => null, // Return instead a promise containing one movie,
   addOne: info => null, // Make the request to add amovie and return a promise containing the added movie,
   editOne: (id, info) => null, // Make the request to edit a movie and return a promise containing the edited movie,
