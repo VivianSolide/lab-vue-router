5fa4baa7a7a8ba9cf23222f7023424f87ff76954
VivianSolide <vivian.sarazin@gmail.com>
2018-02-21 11:45:00 +0100

    iteration #0

--- api.js
+++ api.js
@@ -16,11 +16,22 @@ const api = { // Return instead a promise containing all the movies,
     })
   }, 
     
-
-
-
-  getOne: id => null, // Return instead a promise containing one movie,
-  addOne: info => null, // Make the request to add amovie and return a promise containing the added movie,
+  getOne: id => {
+    return service
+    .get('/:id')
+    .then(movie => {
+      return movie;
+      console.log(movies);
+    })
+    .catch(err => {
+      console.error(err);
+      throw err;
+    })
+  }, // Return instead a promise containing one movie,
+  addOne: info => {
+    // return service
+    // .get()
+  }, // Make the request to add amovie and return a promise containing the added movie,
   editOne: (id, info) => null, // Make the request to edit a movie and return a promise containing the edited movie,
   deleteOne: id => null, // Make the request to delete a movie
 };
